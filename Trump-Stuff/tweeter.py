ecd4603929533dff7158ce4c01ef48ba97cb3272
sdgloss <63432385+sdgloss@users.noreply.github.com>
2021-12-01 12:50:08 -0600

    Delete Trump-Stuff directory

--- Trump-Stuff/tweeter.py
+++
@@ -1,7 +0,0 @@
-from engine import GrammarEngine
-
-def tweeter():
-  engine = GrammarEngine(file_path="corpora/Sentence.txt")
-  print("@RealDonaldTrump tweeting here:")
-  for i in range(5):
-      print(engine.generate(start_symbol_name="1"))
